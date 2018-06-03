978c09206de9389b3efe948a8a61e440639cf0ee
bingka.wang <bingka.wang@yiducloud.cn>
2018-06-03 23:27:03 +0800

    '1'

--- class4.py
+++ class4.py
@@ -8,8 +8,8 @@ class Cola():
     def openCola(self):
         print('open cola')
     #构造函数，默认没有参数,改变公有变量默认的值
-    def __init__(self,aname):
-        self.name = aname
+    def __init__(self):
+        self.name = '名字'
     #类方法,cls代表当前类本身
     @classmethod
     def test2(cls):
@@ -18,8 +18,8 @@ class Cola():
     @staticmethod
     def test3():
         print('this is a static method')
-
-newCola = Cola('SundyCola')
+newCola = Cola()
+newCola.name = 'SundyCola'
 print(newCola.name)
 Cola.test2()
 Cola.test3()
@@ -30,22 +30,41 @@ class CocaCola(Cola):
     name = 'CocaCola'
     def openCola(self):
         print('open Cocala')
+    #子类想要继续使用父类的构造方法
+    # 有两种方法能达到这个目的：调用超类构造方法的未绑定版本，或者使用super函数。
+    def __init__(self):
+        Cola.__init__(self)
+        self.manfu = '可口可乐公司'
 class PepsiCola(Cola):
     color = 'blue'
     name = 'PepsiCola'
     def openCola(self):
         return 'open PepsiCola'
+    def __init__(self):
+        super(PepsiCola, self).__init__()
+        self.manfu = '百事可乐公司'
 
 #子类可继承父类的变量和函数，但也可以重新定义
-aCola =CocaCola('CocaCola')
+aCola =CocaCola()
+aCola.name = 'CocaCola'
+aCola.manfu = '可口可乐公司厂1'
 print(aCola.name)
 print(aCola.color)
-print(aCola.openCola())
-bCola = PepsiCola('PepsiCola')
+
+bCola = PepsiCola()
+bCola.name = 'PepsiCola1'
+bCola.manfu = '百事可乐公司厂2'
 print(bCola.name)
 print(bCola.color)
+
+cCola = PepsiCola()
+
 print(aCola.openCola())
 print(bCola.openCola())
+print(aCola.manfu)
+print(bCola.manfu)
+print(cCola.manfu)
+
 
 #type,查对象的数据类型
 print(type(12345))
---
+++ tkinter3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/3 22:30
+# @Author  : bingka.wang 
+# @Email   : wangbingka@126.com
+
+# 1、root，界面,ui
+# 2、widet,ui主键
+# 3、WM，窗体显示，以及叠加方式
+# 4、event，输入事件，类似鼠标和键盘，button传导操作方式，
+# 状态事件
+# 管理事件，类似窗体的生命周期消亡
