234a20eb5bb7596cadae3080873075a34c5a70e6
bingka.wang <bingka.wang@yiducloud.cn>
2018-07-04 20:38:30 +0800

    ''

---
+++ javascript/1.2网页中嵌入脚本.js
@@ -0,0 +1,11 @@
+// var 定义变量使用，可以定义成字符串、数字、数组等
+// "&&"表示"并且"，“||”表示“或”。
+
+var weather = '下雨';tempreture = 26;
+if ((weather === '晴天')||(tempreture >= 26)) {
+    alert('心情很棒！')
+} if (weather == '阴天') {
+    alert('忧郁')
+} else {
+    alert('心情糟糕')
+}
