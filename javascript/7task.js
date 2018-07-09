9d5aa746b97f3e542cf31e8f263fe3206baf89ba
wangbingka <wangbingka@hotmail.com>
2018-07-10 00:03:59 +0800

    ''

--- javascript/7task.js
+++ javascript/7task.js
@@ -1,19 +1,31 @@
-windows.load(){
-    function showForm() {
+window.onload =function () {
     var create = document.getElementById("login");
-    // var bg = document.getElementsByClassName("background")[0];
-    var loginPage = document.getElementById("login-page");
-    var links = document.getElementsByClassName("close");
-    for(var i=0;i<links.length;i++) {
-        links[i].onclick = function() {
-        loginPage.style.display = "none";
-        // bg.style.display = "none";
-        }
-    }
-    create.onclick = function() {
+    var closeLogin = document.getElementById("close");
+    function showLogin(){
+        var loginPage = document.getElementById("login-page");
         loginPage.style.display = "block";
-        // bg.style.display = "block";
-    }
+    };
+    function hideLogin(){
+        var loginPage = document.getElementById("login-page");
+        loginPage.style.display = "none";
+    };
+    create.addEventListener('click',showLogin,false);
+    closeLogin.addEventListener('click',hideLogin,false);
 
-}
-}
+
+    var veryfy = document.getElementById('veryfy');
+    function check(){
+　　 var filter = /(^(1[35678]\d{9})$)|(^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$)/i;
+ 　　var zhangHao = document.getElementById('zhanghao').value;
+ 　　if(zhangHao === ""){ //输入不能为空
+ 　　　　alert("账号不能为空!");
+　　　　return false;
+　　}else if(!filter.test(zhangHao)){
+ 　　　　alert("账号格式错误!");
+　　　　return false;
+　　}else{
+　　　　return true;
+　　}
+ }
+    veryfy.addEventListener('click',check,false);
+};
