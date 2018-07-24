fbdd9d6d8007119f3fedbdcf153deed0b38c3eb4
wangbingka <wangbingka@hotmail.com>
2018-07-25 00:03:33 +0800

    '1'

--- movie/doubanmovie/urls.py
+++ movie/doubanmovie/urls.py
@@ -13,16 +13,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+客户端，常见的是浏览器，输入电影名
+    点击搜索，是在发出一种请求
+到服务器段，web应用
+    1、服务器端收到客户端的请求内容
+    2、查询表格，或者数据库，exce;,mysql等
+    3、获取表格信息，查询过程
+    4、返回查询到的数据，发还给客户端
+客户端收到服务器发回的数据，一次服务完成
+常用框架 django
+"""
+
+
 from django.contrib import admin
 from django.urls import include, path
 
 #python manage.py startproject projectName ,新建一个项目
 #python manage.py startapp appName，要在项目目录下新建模块
+#python manage.py runserver 运行这个服务器起来
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('polls.urls')),
+    path('movie/', include('movie.urls')),
 ]
+
+#http://127.0.0.1:8000/movie/,网址栏首先输入这一个，然后根据后面的urls内容继续往后输入
+
 #何时使用 include(),当包括其它 URL 模式时你应该总是使用 include() ， admin.site.urls 是唯一例外。
 #通过前面的'admin'或者'movie'，进入一个界面，除非是admin.site.urls，其他都应该用include来引导
 
---
+++ movie/movie/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request,name):
+    #name是需要在网址中输入的内容，可以随便输入
+
+    #获取要查询的电影
+    movie = name
+    print(movie)
+    #打开excel,数据存储库
+    lines = ['蛮荒故事 Relates salvajes','肖申克的救赎 Relates salvajes']
+
+    #查询
+    for line in lines:
+        if movie in line:
+            # return HttpResponse(line)
+            context = {'tip': line,'sayhi': 'hello!'}
+            return render(request, 'movie/index.html', context)
+    fo =  'for'
+
+    #传递变量，可以一次传递多个变量
+    """
+    1、index.html中使用方式： {{ tip }}
+    2、如果变量不多，可以直接放入render作为第三个参数
+       
+    """
+    # context = }
+    # return render(request, 'movie/index.html', context)
+
+    return render(request, 'movie/index.html', {'tip': '没有该电影', 'sayhi': '很遗憾，我们会尽快添加！'})
+
+    # 怎么使用html文件
+    """
+    1、如果需要运行html,必须把建立的app，加入setting.py的‘NSTALLED_APPS’列表中
+    2、在app下面，建立templates文件夹，直接放，直接只用'index.html',或者继续加子目录，前面就需要加上路径
+    
+    """
