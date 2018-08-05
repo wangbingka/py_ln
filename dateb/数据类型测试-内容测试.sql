0c84aa7a2ed74be1149416f39ef756330285f0b9
wangbingka <wangbingka@hotmail.com>
2018-08-06 04:17:59 +0800

    '1'

--- dateb/数据类型测试-内容测试.sql
+++ dateb/数据类型测试-内容测试.sql
@@ -1,4 +1,4 @@
---测试整型
+一、--测试整型
 CREATE TABLE test1(
 num1 TINYINT,
 num2 SMALLINT,
@@ -37,3 +37,26 @@ INSERT test3 VALUES(1,1,1,1,1)
 num1 | num2  | num3     | num4       | num5 |
 +------+-------+----------+------------+------+
 |  001 | 00001 | 00000001 | 0000000001 |    1 |
+
+二、测试浮点型:
+CREATE TABLE IF NOT EXISTS test4(
+num1 FLOAT(6,2),
+num2 DOUBLE(6,2),
+num3 DECIMAL(6,2)
+);
+INSERT test4 VALUES(3.1415,3.1415,3.1415);
+INSERT test4 VALUES(3.1415,3.1495,3.14185);
+
+1、插入的浮点数，超过规定的位数，会四舍五入；
+2、FLOAT,存储的是数字,如果转换为字符串时，并不是严格的'3.14'，而是无限接近于'3.14'
+   DOUBLE，双精度，可以等于字符串'3.14'
+   而DECIMAL，定点数，对精度要求比较高，存储的是字符换，和'3.14'严格相等
+   
+测试：
+SELECT * FROM test4 where num1 = '3.14';
+结果为空
+
+SELECT * FROM test4 where num2 = '3.14' and num3 = '3.14';
+结果有值。
+
+
