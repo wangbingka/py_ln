302e084c13dfd83feb77c940cb54704eb862e7ef
wangbingka <wangbingka@hotmail.com>
2018-09-12 23:29:22 +0800

    '1'

--- 爬虫基础pachong/xlrd_xlwt20180905.py
+++ 爬虫基础pachong/xlrd_xlwt20180905.py
@@ -18,6 +18,28 @@ filename1 = filepath+'\\'+filename
 
 print(filename1)
 
+# 打开excel文件
 data = xlrd.open_workbook(filename)#文件名以及路径，如果路径或者文件名有中文给前面加一个r转化原生字符。
 
-print(data)
+# 获取所有sheet的名字，返回一个列表
+sheets = data.sheet_names()
+print(sheets)
+
+#  通过sheet的索引，获取对应的sheet表内容，返回的是一个类，
+#  对应的方法有多个，name指sheet的名称，ncols，列数，nrows，行数
+
+sheet1 = data.sheet_by_index(0)
+sheet2 = data.sheet_by_index(1)
+sheet3 = data.sheet_by_index(2)
+print(sheet1.name,sheet1.ncols,sheet1.nrows)
+print(sheet2.name,sheet2.ncols,sheet2.nrows)
+print(sheet3.name,sheet3.ncols,sheet3.nrows)
+
+
+print(sheet1.cell(1,0))
+print(sheet1.cell(1,0).value)
+rows = sheet1.row_values(1)
+cols = sheet2.col_values(2)
+
+print(rows)
+print(cols)
